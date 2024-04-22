18aac62ec39a9a76d4e0da9919bfae117d0671e9
NefersQ <nefersq@gmail.com>
2024-04-22 16:12:25 +0300

    Clean code in controllers

--- server/src/main/java/com/billbasher/controllers/TokenController.java
+++ server/src/main/java/com/billbasher/controllers/TokenController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/token")
 public class TokenController {
-
     @Autowired
     private AuthService authService;
 
